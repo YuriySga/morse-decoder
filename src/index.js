d5b590f95cef441419048e7a72cf1a528ac1e60e
YuriySga <ess1928@yandex.ru>
2020-03-15 19:16:07 +0400

    feat: morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    let temp = [];
+    let arrArr = [];
+    let arrDotsDashes = [];
+    for (i=0; i < arr.length+1; ++i){ 
+        if (i%10 == 0 && i !== 0) {        
+                arrArr.push(temp.slice());      
+                temp.length=0;       
+        }   
+        temp.push(arr[i]);
+    }
+
+    temp.length=0;
+    arrArr.forEach(item => {
+        item.reduce((prev, item1, index) => {                                 
+            if ((index+1)%2 == 0) {
+                if (prev+item1 == 10) {
+                    temp.push('.');
+                } else if (prev+item1 == 11) {
+                    temp.push('-');
+                } else if (prev+item1 == '**') { 
+                    if (temp[temp.length-1] != ' ') temp.push(' ');
+                } else if (prev+item1 == '00') {
+                    //nop;
+                } 
+            }   else if ((index+1)%1 == 0) {
+                return prev = item1;     
+            }
+        }, 0);
+
+        arrDotsDashes.push((MORSE_TABLE[temp.join('')] == undefined) ? ' ' : MORSE_TABLE[temp.join('')]);
+        temp.length=0;
+    });
+
+    return arrDotsDashes.join('');
 }
 
 module.exports = {
